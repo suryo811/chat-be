0a2f59370a2dde9d7b601f189034fb98d08d6f92
suryoday <suryodaykaushik@gmail.com>
2025-03-25 20:41:10 +0530

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import type { Application, Request, Response } from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+
+const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// health check
+app.get('/health', (req: Request, res: Response) => {
+  res.send('Server is up and running!');
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} 🚀`);
+});
